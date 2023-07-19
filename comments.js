173251703cc8b18fbadeac2ee194e455b16ec87f
amineker123 <63362755+amineker123@users.noreply.github.com>
2023-07-19 11:54:31 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,68 @@
+// Create web server
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.write('Hello World');
+//     res.end();
+//   }
+//   if (req.url === '/api/courses') {
+//     res.write(JSON.stringify([1, 2, 3]));
+//     res.end();
+//   }
+// });
+// server.listen(3000);
+// console.log('Listening on port 3000...');
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+// // Register a listener
+// emitter.on('messageLogged', function() {
+//   console.log('Listener called');
+// });
+// // Raise an event
+// emitter.emit('messageLogged');
+// const EventEmitter = require('events');
+// const Logger = require('./logger');
+// const logger = new Logger();
+// // Register a listener
+// logger.on('messageLogged', function(arg) {
+//   console.log('Listener called', arg);
+// });
+// logger.log('message');
+// const EventEmitter = require('events');
+// const Logger = require('./logger');
+// const logger = new Logger();
+// // Register a listener
+// logger.on('messageLogged', function(arg) {
+//   console.log('Listener called', arg);
+// });
+// logger.log('message');
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.write('Hello World');
+//     res.end();
+//   }
+//   if (req.url === '/api/courses') {
+//     res.write(JSON.stringify([1, 2, 3]));
+//     res.end();
+//   }
+// });
+// server.listen(3000);
+// console.log('Listening on port 3000...');
+// const EventEmitter = require('events');
+// const Logger = require('./logger');
+// const logger = new Logger();
+// // Register a listener
+// logger.on('messageLogged', function(arg) {
+//   console.log('Listener called', arg);
+// });
+// logger.log('message');
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   if (req.url === '/') {
+//     res.write('Hello World');
+//     res.end();
+//   }
+//   if (req.url === '/api/courses') {
+//     res.write(JSON.stringify([1
+
